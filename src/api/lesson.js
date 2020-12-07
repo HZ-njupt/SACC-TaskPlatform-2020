767bd56cb808f5ef582e21fc9dce7c7330166e03
xuan-659 <953579022@qq.com>
2020-12-07 20:30:16 +0800

    add:lesson api

--- src/api/lesson.js
+++ src/api/lesson.js
@@ -1,17 +1,23 @@
 import request from '@/utils/request'
-import axios from 'axios'
 
 export function addLesson(data) {
+  console.log(data);
   return request({
-    url: 'http://127.0.0.1:4523/mock/361581/lesson/addLesson',
+    // url: 'http://127.0.0.1:4523/mock/361581/lesson/addLesson',
+    url: 'api/lesson/addLesson',
     method: 'post',
-    data
+    // dataType:"json",
+    header:{
+      "content-type":'application/json'
+    },
+    data:JSON.stringify(data)
   })
 }
 
 export function updateLesson(data) {
   return request({
-    url: 'http://127.0.0.1:4523/mock/361581/lesson/update',
+    // url: 'http://127.0.0.1:4523/mock/361581/lesson/update',
+    url: 'api//lesson/update',
     method: 'post',
     data
   })
@@ -19,31 +25,32 @@ export function updateLesson(data) {
 
 export function getLesson(data) {
   return request({
-    url: 'http://127.0.0.1:4523/mock2/361581/3888886',
+    // url: 'http://127.0.0.1:4523/mock2/361581/3888886',
+    url: 'api/lesson/getByGroupId?groupId='+data,
     method: 'post',
-    data
+    // data
   })
 }
 
 export function deleteLesson(data) {
   return request({
-    url: 'http://127.0.0.1:4523/mock/361581/lesson/delete',
+    // url: 'http://127.0.0.1:4523/mock/361581/lesson/delete',
+    url: 'api/lesson/delete?id='+data,
     method: 'post',
-    data
   })
 }
 
 export function updateLessonURL(data) {
   return request({
-    url: 'http://127.0.0.1:4523/mock/361581/updateurl',
+    url: 'http://127.0.0.1:4523/mock/361581/updateurl?lessonId='+data,
     method: 'post',
-    data
   })
 }
 
 export function isLiveUrl(data) {
   return request({
-    url: 'http://127.0.0.1:4523/mock/361581/setLessonType',
+    // url: 'http://127.0.0.1:4523/mock/361581/setLessonType',
+    url: 'api/setLessonType',
     method: 'post',
     data
   })
